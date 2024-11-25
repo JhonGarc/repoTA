2910b3865b20480b9fd97f5116a1b0e7cc5e8c99
JhonGarc <Jhon61631@gmail.com>
2024-11-25 10:08:02 -0500

    first commit

---
+++ src/main/java/com/example/demo/Auth/AuthService.java
@@ -0,0 +1,56 @@
+package com.example.demo.Auth;
+
+
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Service;
+
+import com.example.demo.User.Role;
+import com.example.demo.User.User;
+import com.example.demo.User.UserRepository;
+
+import com.example.demo.Jwt.JwtService;
+
+
+import lombok.RequiredArgsConstructor;
+
+@Service
+@RequiredArgsConstructor
+public class AuthService {
+
+    private final UserRepository userRepository;
+    private final JwtService jwtService;
+    private final PasswordEncoder passwordEncoder;
+    private final AuthenticationManager authenticationManager;
+
+    public AuthResponse login(LoginRequest request) {
+        authenticationManager.authenticate(new UsernamePasswordAuthenticationToken(request.getUsername(), request.getPassword()));
+        UserDetails user=userRepository.findByUsername(request.getUsername()).orElseThrow();
+        String token=jwtService.getToken(user);
+        return AuthResponse.builder()
+            .token(token)
+            .build();
+
+    }
+
+    public AuthResponse register(RegisterRequest request) {
+        User user = User.builder()
+            .username(request.getUsername())
+            .password(passwordEncoder.encode( request.getPassword()))
+            .firsname(request.getFirstname())
+            .lastname(request.lastname)
+            .country(request.getCountry())
+            .role(Role.USER)
+            .build();
+
+        userRepository.save(user);
+
+        return AuthResponse.builder()
+            .token(jwtService.getToken(user))
+            .build();
+        
+    }
+
+}
